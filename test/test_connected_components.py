33b02962ee241ea01ec2afa1da063ef35b8193b2
Ethan Tu <ethantu@microsoft.com>
2023-09-17 09:10:10 +0000

    fix tests

--- test/test_connected_components.py
+++ test/test_connected_components.py
@@ -1,3 +1,6 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import count
+
 from receiptprocessor.graphframes.connected_components import ConnectedComponents
 
 
@@ -8,7 +11,7 @@ def test_ConnectedComponents(spark_session, cleaned_data, component_vertice_mapp
         edge_columns=edge_columns, vertice_columns=vertice_columns
     )
     connected_components = connected_components_transformer.transform(cleaned_data)
-    assert connected_components.collect() == component_vertice_mapping.collect()
+    assert are_component_counts_equal(connected_components, component_vertice_mapping)
 
 
 def test_ConnectedComponents_set_parameter_in_transform(
@@ -24,7 +27,7 @@ def test_ConnectedComponents_set_parameter_in_transform(
             connected_components_transformer.vertice_columns: vertice_columns,
         },
     )
-    assert connected_components.collect() == component_vertice_mapping.collect()
+    assert are_component_counts_equal(connected_components, component_vertice_mapping)
 
 
 def test_ConnectedComponents_setParameter(spark_session, cleaned_data):
@@ -33,3 +36,20 @@ def test_ConnectedComponents_setParameter(spark_session, cleaned_data):
         connected_components_transformer.transform(cleaned_data)
     except ValueError as e:
         assert str(e) == "EdgeColumns cannot be empty"
+
+
+def are_component_counts_equal(df: DataFrame, expected_df: DataFrame) -> bool:
+    component_count_column = "component_count"
+    actual_component_counts = (
+        df.groupBy("component")
+        .agg(count("*").alias(component_count_column))
+        .select(component_count_column)
+        .orderBy(component_count_column)
+    )
+    expected_component_counts = (
+        expected_df.groupBy("component")
+        .agg(count("*").alias(component_count_column))
+        .select(component_count_column)
+        .orderBy(component_count_column)
+    )
+    return actual_component_counts.collect() == expected_component_counts.collect()
